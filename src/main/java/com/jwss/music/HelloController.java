0a6173798f17f6806e2db81b00c2d3998b55c3db
CoCoTea <572315466@qq.com>
2022-01-22 04:27:20 +0800

    test MediaPlayer

--- src/main/java/com/jwss/music/HelloController.java
+++ src/main/java/com/jwss/music/HelloController.java
@@ -2,13 +2,23 @@ package com.jwss.music;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
+import java.io.File;
+
+/**
+ * @author jwss
+ */
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
+        Media media = new Media(new File("F:\\music\\网易云\\MYTH & ROID - HYDRA.mp3").toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.setAutoPlay(true);
         welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,7 @@
 module com.jwss.music {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
-
-    requires eu.hansolo.tilesfx;
+    requires javafx.media;
 
     opens com.jwss.music to javafx.fxml;
     exports com.jwss.music;
