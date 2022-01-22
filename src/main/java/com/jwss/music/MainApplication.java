605d05c86d9c730ad9a5b40fa29cc38eb308c36f
CoCoTea <572315466@qq.com>
2022-01-22 14:45:25 +0800

    test MediaPlayer finish

--- src/main/java/com/jwss/music/MainApplication.java
+++ src/main/java/com/jwss/music/MainApplication.java
@@ -6,15 +6,17 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 /**
  * @author jwss
  */
-public class HelloApplication extends Application {
+public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1022, 670);
+        scene.setUserAgentStylesheet(Objects.requireNonNull(MainApplication.class.getResource("main-view.css")).toExternalForm());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
---
+++ src/main/java/com/jwss/music/service/IMediaPlayerService.java
@@ -0,0 +1,8 @@
+package com.jwss.music.service;
+
+/**
+ * 播放器服务接口
+ * @author jwss
+ */
+public interface IMediaPlayerService {
+}
