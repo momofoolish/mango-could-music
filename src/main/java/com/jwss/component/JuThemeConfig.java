de9f8581316dc9c60974a544e7687cb61ad4e6c1
CoCoTea <572315466@qq.com>
2022-05-11 00:27:21 +0800

    :sparkles: 增加主体配置类

---
+++ src/main/java/com/jwss/component/JuThemeConfig.java
@@ -0,0 +1,65 @@
+package com.jwss.component;
+
+/**
+ * 主题配置类
+ *
+ * @author jwss
+ * @date 2022-5-11 00:06:46
+ */
+public class JuThemeConfig {
+    /**
+     * 主体颜色
+     */
+    public final static String PRIMARY = "#409EFF";
+
+    /**
+     * 成功颜色
+     */
+    public final static String SUCCESS = "#67C23A";
+
+    /**
+     * 警告颜色
+     */
+    public final static String WARNING = "#E6A23C";
+
+    /**
+     * 危险颜色
+     */
+    public final static String DANGER = "#F56C6C";
+
+    /**
+     * 信息颜色
+     */
+    public final static String INFO = "#909399";
+
+    /**
+     * 文字主体颜色
+     */
+    public final static String PRIMARY_TEXT = "#303133";
+
+    /**
+     * 文字次要颜色
+     */
+    public final static String SECOND_TEXT = "#FFFFFF";
+
+    /**
+     *
+     */
+    public final static String PLACEHOLDER = "#C0C4CC";
+
+    /**
+     * 边框基础颜色
+     */
+    public final static String BORDER_BASE = "#DCDFE6";
+
+    /**
+     * 背景颜色-黑
+     */
+    public final static String BACKGROUND_B  = "#000000";
+
+    /**
+     * 背景颜色-白
+     */
+    public final static String BACKGROUND_W  = "#FFFFFF";
+
+}
--- src/main/java/com/jwss/component/button/JuButton.java
+++ src/main/java/com/jwss/component/button/JuButton.java
@@ -1,6 +1,10 @@
 package com.jwss.component.button;
 
+import com.jwss.component.JuThemeConfig;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 
 /**
  * JustUI按钮组件
@@ -9,4 +13,9 @@ import javafx.scene.control.Button;
  * @date 2022-5-2 17:30:30
  */
 public class JuButton extends Button {
+    public JuButton() {
+        this.setBorder(new Border(new BorderStroke(Color.valueOf(JuThemeConfig.BORDER_BASE), BorderStrokeStyle.NONE, new CornerRadii(3), BorderWidths.DEFAULT)));
+        this.setBackground(new Background(new BackgroundFill(Color.valueOf(JuThemeConfig.PRIMARY), new CornerRadii(3), Insets.EMPTY)));
+        this.setTextFill(Color.valueOf(JuThemeConfig.SECOND_TEXT));
+    }
 }
